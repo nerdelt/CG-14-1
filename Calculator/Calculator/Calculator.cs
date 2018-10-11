620e10bbf28ac28bc4945aa0776b31be686b5190
nerdelt <nerdelt@gmail.com>
2018-10-11 18:26:47 -0500

    Simple Calculator First Attempt

---
+++ Calculator/Calculator/Calculator.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace SimpleCalculator
+{
+    class Calculator
+    {
+        //properties for user input to be manipulated later
+        public double FirstNumber { get; set; }
+        public double SecondNumber { get; set; }
+        public string Operator { get; set; }
+        public List<string> Equation = new List<string>();
+
+
+        public void MakeEquation(string inputEquation)
+        {
+            double result = 0;
+
+            //exception in case user enters nothing 
+            if (string.IsNullOrWhiteSpace(inputEquation))
+            {
+                Console.WriteLine("Sorry, there is no equation to calculate");
+            }
+
+            //splits input into smaller string and adds to list
+            Equation = inputEquation.Split(' ').ToList();
+
+            //assigns the operator for later use
+            Operator = Equation[1];
+
+            //if user doesn't enter a valid first number
+            if (!Double.TryParse(Equation[0], out result))
+            {
+                Console.WriteLine("Sorry, your first number is not valid.");
+            }
+
+            //assigns first number 
+            else if (Double.TryParse(Equation[0], out result))
+            {
+                FirstNumber = Double.Parse(Equation[0]);
+            }
+
+            //if user doesn't enter valid operator
+            else if (Operator != "+" && Operator != "-" && Operator != "*" && Operator != "/")
+            {           
+                Console.WriteLine("Operator must be +, -, *, or /");
+            }
+
+            //in case user doesn't enter valid second number
+            if (!Double.TryParse(Equation[2], out result))
+            {
+                Console.WriteLine("Sorry, your second number is not valid.");
+            }
+
+            //assigns second number
+            if (Double.TryParse(Equation[2], out result))
+            {
+                SecondNumber = Double.Parse(Equation[2]);
+            }
+
+
+        }
+
+        public void Calculate()
+        {
+            double result;
+
+            //if user tries to divide by zero 
+            if (Operator == "/" && SecondNumber == 0)
+            {
+                Console.WriteLine("Sorry, cannot divide by zero.");
+            }
+
+            //switch statements to handle all the equations and give proper math results
+            switch (Operator)
+            {
+                case "+":
+                    result = FirstNumber + SecondNumber;
+                    Console.WriteLine(result);
+                    break;
+
+                case "-":
+                    result = FirstNumber - SecondNumber;
+                    Console.WriteLine(result);
+                    break;
+
+                case "*":
+                    result = FirstNumber * SecondNumber;
+                    Console.WriteLine(result);
+                    break;
+
+                case "/":
+                    result = FirstNumber / SecondNumber;
+                    Console.WriteLine(result);
+                    break;
+
+                default:
+                    Console.WriteLine("We're sorry, we cannot compute your equation.");
+                    break;
+            }
+        }
+    }
+}
---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace SimpleCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // lets user know whats going on an asks for input
+            Console.WriteLine("Please enter a simple math equation. \n" +
+                "The equation should include only 2 numbers and one of the following operators in the middle: +, - , *, / \n" +
+                "Some examples include: 3 + 5 or 8 * 2");
+            Console.Write("\nPlease enter your equation: ");
+
+            //turns input into string
+            string inputEquation = Console.ReadLine();
+
+            //creates new calculator object
+            Calculator calculator = new Calculator();
+
+            //breaks down equation in order to calculate
+            calculator.MakeEquation(inputEquation);
+
+            Console.Write("\nYou answer is: ");
+
+            //calculates equation
+            calculator.Calculate();
+
+            Console.ReadLine();
+
+        }
+    }
+}
